36d8095b700ab4cda6bb74886f1c033945cdead8
Johnny Lorang <lorang.johnny@gmail.com>
2018-04-16 15:12:13 +0200

    further development of the class

--- sunpos.cpp
+++ sunpos.cpp
@@ -1,26 +1,37 @@
 #include "sunpos.h"
 #include <math.h>
 
-#define PI 3.14159265
-
 //Definition der Konstruktoren
 Sunpos::Sunpos(float lat, float lon) {
     set_latitude(lat);
     set_longitude(lon);
-    set_woz_sec(0);
-    set_woz_min(0);
-    set_woz_hour(0);
+    init();
 }
 Sunpos::Sunpos() {
     set_latitude(49.96);
     set_longitude(5.93);
-    set_woz_sec(0);
-    set_woz_min(0);
-    set_woz_hour(0);
+    init();
+}
+
+//Definition von init()
+void Sunpos::init() {
+    time inittime;                      // ggf Initialisierung über RTC hinzufügen
+    inittime.hour=0;
+    inittime.min=0;
+    inittime.sec=0;
+    time haref;
+    haref.hour=12;
+    haref.min=0;
+    haref.sec=0;
+
+    set_woz(inittime);
+    set_mez(inittime);
+    set_hourangref(haref);
+
+    set_doy(1);
 }
 
 //Definition Elementfunktionen
-
 void Sunpos::set_latitude(float lat) {
     latitude=lat;
 }
@@ -34,35 +45,17 @@ float Sunpos::get_longitude() {
     return longitude;
 }
 
-void Sunpos::set_woz_sec(int secnow) {
-    woz.sec=secnow;
+void Sunpos::set_woz( time& woznow) {
+    woz=woznow;
 }
-int Sunpos::get_woz_sec() {
-    return woz.sec;
+time Sunpos::get_woz() {
+    return woz;
 }
-void Sunpos::set_woz_min(int minnow) {
-    woz.min=minnow;
+void Sunpos::set_mez( time& meznow) {
+    mez=meznow;
 }
-int Sunpos::get_woz_min() {
-    return woz.min;
-}
-void Sunpos::set_woz_hour(int hournow) {
-    woz.hour=hournow;
-}
-void Sunpos::set_mez_sec(int secnow) {
-    woz.sec=secnow;
-}
-int Sunpos::get_mez_sec() {
-    return woz.sec;
-}
-void Sunpos::set_mez_min(int minnow) {
-    woz.min=minnow;
-}
-int Sunpos::get_mez_min() {
-    return woz.min;
-}
-void Sunpos::set_mez_hour(int hournow) {
-    woz.hour=hournow;
+time Sunpos::get_mez() {
+    return mez;
 }
 void Sunpos::set_doy(int doynow) {
     doy=doynow;
@@ -71,7 +64,32 @@ int Sunpos::get_doy() {
     return doy;
 }
 
+float Sunpos::get_eclipticang() {
+    return eclipticang;
+}
 float Sunpos::get_declang() {
-    declang=eclipticang*sin(365/(2*PI))*get_doy();
+    declang=get_eclipticang()*sin(365/(2*PI))*get_doy();
     return declang;
+}
+void Sunpos::set_hourangref( time& harefnew) {
+    hourangref=harefnew;
+}
+float Sunpos::get_hourang() {
+    //????????
+    hourang=5;                          //Falscher Wert, nur für Testzwecke!!!
+    return hourang;
+}
+float Sunpos::get_elevang() {
+    elevang=deg*asin(sin(rad*latitude)*sin(rad*get_declang())\
+            +cos(rad*latitude)*cos(rad*get_declang())*cos(rad*get_hourang()));
+    return elevang;
+}
+float Sunpos::get_azang() {
+    azang=deg*asin(cos(rad*get_declang())*sin(rad*get_hourang()))\
+          /cos(rad*get_elevang());
+    return azang;
+}
+float Sunpos::get_pvtiltang() {
+    pvtiltang=90-get_elevang();
+    return pvtiltang;
 }
--- sunpos.h
+++ sunpos.h
@@ -1,6 +1,8 @@
 #ifndef SUNPOS_H
 #define SUNPOS_H
 
+#define PI 3.14159265
+
 struct time {
     int sec;
     int min;
@@ -10,44 +12,52 @@ struct time {
 
 class Sunpos {
 private:
+    const float deg=180/PI;
+    const float rad=PI/180;
+
     float latitude;
     float longitude;
 
     time woz;                           // Wahre Ortszeit, Sonnenzeit
     time mez;                           // Lokale Ortszeit
-    int doy;                            // Day of Year; 1. Januar = 0
+    int doy;                            // Day of Year; 1. Januar = 1
 
     const float eclipticang=23.45;      // Ekliptikwinkel der Erde
     float declang;                      // Earth Declination Winkel in Gard
-    float hourang;                      //
+    time  hourangref;                   // Referenzzeit für hourang
+    float hourang;                      // HA - Hour Angle
+    float elevang;                      // Elevation Angle Beta @ Solar Time
+    float azang;                        // Solar Angle Azimut Angle @ Solar time
+    float pvtiltang;                    // PV Panel tilt Angle, lokal, horizontal gegenüber Boden
 
 public:
   //Deklaration der Konstruktoren
     Sunpos(float, float);               // (latitude, longitude)
     Sunpos();                           // default ist Wiltz
+  //Init
+    void init();
   //Deklaration der Elementfunktionen
     void set_latitude(float);
     float get_latitude();
     void set_longitude(float);
     float get_longitude();
 
-    void set_woz_sec(int);
-    int get_woz_sec();
-    void set_woz_min(int);
-    int get_woz_min();
-    void set_woz_hour(int);
-    int get_woz_hour();
-    void set_mez_sec(int);
-    int get_mez_sec();
-    void set_mez_min(int);
-    int get_mez_min();
-    void set_mez_hour(int);
-    int get_mez_hour();
+    void set_woz( time& woznow);
+    time get_woz();
+    void set_mez( time& meznow);
+    time get_mez();
     void set_doy(int);
     int get_doy();
 
     float get_eclipticang();
     float get_declang();
+    void set_hourangref ( time& harefnew);
+    float get_hourang();
+    float get_elevang();
+    float get_azang();
+    float get_pvtiltang();
+
+
 };
 
 #endif // SUNPOS_H
